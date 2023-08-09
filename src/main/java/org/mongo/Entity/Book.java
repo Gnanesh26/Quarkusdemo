1ad4037b046f547c121b9d04b05234834c572db3
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2023-08-09 11:50:42 +0530

    intial commit

---
+++ src/main/java/org/mongo/Entity/Book.java
@@ -0,0 +1,74 @@
+package org.mongo.Entity;
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import io.quarkus.mongodb.panache.common.MongoEntity;
+import jakarta.json.bind.annotation.JsonbProperty;
+
+@MongoEntity(collection="books")
+public class Book extends PanacheMongoEntity {
+    @JsonbProperty("title")
+    public String title;
+
+    @JsonbProperty("author")
+    public String author;
+
+    @JsonbProperty("pages")
+    public int pages;
+
+    @JsonbProperty("genres")
+    public String genres;
+
+    @JsonbProperty("rating")
+    public double rating;
+
+    public Book(String title, String author, int pages, String genres, double rating) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.genres = genres;
+        this.rating = rating;
+    }
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
+
---
+++ src/main/java/org/mongo/Resource/BookResource.java
@@ -0,0 +1,39 @@
+package org.mongo.Resource;
+
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.bson.types.ObjectId;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.mongo.Entity.Book;
+
+import java.util.List;
+
+@Path("/books")
+public class BookResource {
+
+    @GET
+    @Operation(summary = "Get a list of books")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Book> getBooks() {
+        return Book.listAll();
+    }
+
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Create a new book")
+    public Response createBook(Book book) {
+        book.persist();
+        return Response.status(Response.Status.CREATED).entity(book).build();
+    }
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Get a book by ID")
+    public Book getBookById(@PathParam("id") String id) {
+        return Book.findById(new ObjectId(id));
+    }
+}
